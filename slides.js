8ab8459bf32c29c7bb3850284c6caee4ba054171
pruhjects <cyaaaaaaaaan@gmail.com>
2023-10-27 13:34:53 +0800

    first commit

---
+++ slides.js
@@ -0,0 +1,36 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("img-slides");
+    let dots = document.getElementsByClassName("demo");
+    let dots2 = document.getElementsByClassName("image")
+    let captionText = document.getElementById("caption");
+    let imageText = document.getElementById("places");
+
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+    captionText.innerHTML = dots[slideIndex-1].alt;
+    imageText.innerHTML = dots2[slideIndex-1].alt;
+
+    console.log(dots2[slideIndex-1]);
+  }
